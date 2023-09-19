352ba695ac7a6c45faacf50aa852c80e0a6f62dc
Tiago Ferreira <tiago@okteto.com>
2023-09-19 14:47:10 +0200

    Total component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -14,6 +14,8 @@ const Content = (props) => (
 </>
 )
 
+const Total = (props) => <p>Number of exercises {props.total}</p>
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -27,7 +29,7 @@ const App = () => {
     <div>
       <Header course={course}/>
       <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
