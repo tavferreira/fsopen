36e5b6f7f00c6fa44d43c6666e715bfc220136e4
Tiago Ferreira <tiago@okteto.com>
2023-09-19 14:43:55 +0200

    Content component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,19 @@
 const Header = ({course}) => <h1>{course}</h1>
 
+const Content = (props) => (
+<>
+  <p>
+    {props.part1} {props.exercises1}
+  </p>
+  <p>
+    {props.part2} {props.exercises2}
+  </p>
+  <p>
+    {props.part3} {props.exercises3}
+  </p>
+</>
+)
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -12,15 +26,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
