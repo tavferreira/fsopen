59b9bc9c6172d49e6ed0f3db6eb05d4e2c71d55d
Tiago Ferreira <tiago@okteto.com>
2023-09-20 21:33:35 +0200

    1.8: unicafe step3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,16 +12,11 @@ const Statistic = (props) => (
   <div>{props.name} {props.value}</div>
 )
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+const Statistics = ({good, neutral, bad}) => {
   const total = good + neutral + bad
 
   const average = () => {
-    return ((good*1) + (neutral*0) + (bad*-1)) / total 
+    return total !== 0 ? ((good*1) + (neutral*0) + (bad*-1)) / total : 0
   }
 
   const positivePercentage = () => {
@@ -30,6 +25,21 @@ const App = () => {
 
     return (good/total)*100
   }
+  return (
+  <>  
+    <div>good {good}</div>
+    <div>neutral {neutral}</div>
+    <div>bad {bad}</div>
+    <div>average {average()}</div>
+    <div>positive {positivePercentage()} %</div>
+  </>
+)}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
@@ -38,11 +48,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral+1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Header text="statistics" />
-      <Statistic name="good" value={good} />
-      <Statistic name="neutral" value={neutral} />
-      <Statistic name="bad" value={bad} />
-      <Statistic name="average" value={average()} />
-      <Statistic name="positive" value={`${positivePercentage()} %`} />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
