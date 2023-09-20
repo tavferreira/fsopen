248f08c0837406e2dd2187312b04e4094da0f5b1
Tiago Ferreira <tiago@okteto.com>
2023-09-20 21:12:18 +0200

    1.7: unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -18,6 +18,19 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good + neutral + bad
+
+  const average = () => {
+    return ((good*1) + (neutral*0) + (bad*-1)) / total 
+  }
+
+  const positivePercentage = () => {
+
+    if(total === 0) return 0
+
+    return (good/total)*100
+  }
+
   return (
     <div>
       <Header text="give feedback" />
@@ -28,6 +41,8 @@ const App = () => {
       <Statistic name="good" value={good} />
       <Statistic name="neutral" value={neutral} />
       <Statistic name="bad" value={bad} />
+      <Statistic name="average" value={average()} />
+      <Statistic name="positive" value={`${positivePercentage()} %`} />
     </div>
   )
 }
