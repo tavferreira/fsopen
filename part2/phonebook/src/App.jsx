3c0120d22a1a14fe08c4a9281d20692b63d3ba5f
Tiago Ferreira <tiago@okteto.com>
2023-09-23 18:30:39 +0200

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,19 +3,22 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const personExists = (name) => persons.some(person => person.name === name)
 
   const addPerson = (event) => {
     event.preventDefault()
-    personExists(newName) ? alert(`${newName} is already added to phonebook`): setPersons(persons.concat({name: newName}))
+    personExists(newName) ? alert(`${newName} is already added to phonebook`): setPersons(persons.concat({name: newName, number: newNumber}))
     setNewName('')
+    setNewNumber('')
   }
 
   const handlePersonChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
   
   return (
     <div>
@@ -23,6 +26,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handlePersonChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
