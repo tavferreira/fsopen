a7515580b892b699b5bcfe5b3ec72d2237ee1dec
Tiago Ferreira <tiago@okteto.com>
2023-09-28 23:01:37 +0200

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list-helper.js
+++ part4/bloglist/utils/list-helper.js
@@ -6,6 +6,11 @@ const totalLikes = (blogs) => {
   return blogs.reduce((accumulator, currentValue) => accumulator + currentValue.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if(blogs.length === 0) return null
+  return blogs.reduce((max,blog) => max.likes > blog.likes ? max : blog)
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
