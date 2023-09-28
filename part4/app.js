8006231e323eaf5beeb19adc0daacaabbdecb822
Tiago Ferreira <tiago@okteto.com>
2023-09-28 10:35:53 +0200

    add middleware

--- part4/app.js
+++ part4/app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
-
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -23,7 +23,11 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
