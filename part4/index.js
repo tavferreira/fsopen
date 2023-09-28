50e052ba77266c16edee422237f57bd8bf38bbb1
Tiago Ferreira <tiago@okteto.com>
2023-09-28 10:33:05 +0200

    move to app

--- part4/index.js
+++ part4/index.js
@@ -1,31 +1,6 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app') // the actual Express application
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
