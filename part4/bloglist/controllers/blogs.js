1d4b003b435fbc43a3ab8d6c9c01f7a452d55292
Tiago Ferreira <tiago@okteto.com>
2023-10-01 00:07:10 +0200

    4.15: bloglist expansion, step3

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,52 +1,53 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  await Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const { body } = request
+
+  const user = await User.findById(body.userId)
+
   const blogData = {
     ...body,
+    user: user.id,
     likes: body.likes || 0
   }
   const blog = new Blog(blogData)
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
   const id = request.params.id
 
-  await Blog
-    .findByIdAndDelete(id)
-    .then((deletedNote) => {
-      if (deletedNote) {
-        response.status(204).end()
-      } else {
-        response.status(404).end()
-      }
-    })
+  const deletedNote = await Blog.findByIdAndDelete(id)
+
+  if (deletedNote) {
+    response.status(204).end()
+  } else {
+    response.status(404).end()
+  }
 })
 
 blogsRouter.put('/:id', async (request, response) => {
   const { likes } = request.body
 
-  await Blog
-    .findByIdAndUpdate(request.params.id, { likes }, { new: true })
-    .then(updatedNote => {
-      if (updatedNote) {
-        response.json(updatedNote)
-      } else {
-        response.status(404).end()
-      }
-    })
+  const updatedNote = await Blog.findByIdAndUpdate(request.params.id, { likes }, { new: true })
+
+  if (updatedNote) {
+    response.json(updatedNote)
+  } else {
+    response.status(404).end()
+  }
 })
 
 module.exports = blogsRouter
