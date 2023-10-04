909148e67c1da7130ec5482555d999ceb3263917
Tiago Ferreira <tiago@okteto.com>
2023-10-04 22:42:13 +0200

    5.7 Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+  console.log(blog.user)
+  return (
+  <div style={blogStyle}>
+    {blog.title} {blog.author} <button onClick={toggleVisibility}>view</button>
+    <div style={{ display: visible ? '' : 'none'}}>
+      <div>{blog.url}</div>
+      <div>likes {blog.likes}</div>
+      {blog.user && blog.user.name && <div>blog.user.name</div>}
+    </div>
   </div>  
-)
+)}
 
 export default Blog
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-
 let token = null
 
 const setToken = newToken => {
