4afac3cf4b36ba8140ff8c75b9bdc70c58ae3e43
Tiago Ferreira <tiago@okteto.com>
2023-10-06 22:02:05 +0200

    5.17: bloglist end to end testing, step1

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,7 +1,14 @@
 describe('Blog app', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:5173')
+  })
+
+  it('Login form is shown', function() {
     cy.contains('blogs')
-    cy.contains('Note app, Department of Computer Science, University of Helsinki 2023')
+    cy.contains('login').click()
+    cy.get('#username')
+    cy.get('#password')
+    cy.contains('cancel')
   })
 })
